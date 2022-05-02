cddf754cbab185e8c5067eef9e6ade394b4da73c
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-02 09:03:42 -0300

    commit na API

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World com React Celke now!</h1>')
 })
  
-app.get('/contatos', (req, res) => {
-
+app.get('/contatos/:id', (req, res) => {
+    const {id} = req.params;
+    const {job} = req.query;
 
     return res.json({
-    'nome': 'RENATO ALVES SOARES',
-    'email': 'renatoguara2020@gmail.com',
-    'idade': 50
+    id:id,
+    nome: 'RENATO ALVES SOARES',
+    email: 'renatoguara2020@gmail.com',
+    job: job,
+    idade: 50
   })
 });
+app.post('/contato', (req, res)=> {
+
+  return res.json({
+    nome: 'NATHAN BARBOSA SOARES',
+    email: 'nathanguara2020@gmail.com',
+    job: 'React and Node JS Developer',
+    idade: 4
+
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
