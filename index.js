a3df1a737bc1fbd5a03f0713d0726e24eef19fa7
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-02 09:18:18 -0300

    commit changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('<h1>Hello World com React Celke now!</h1>')
 })
